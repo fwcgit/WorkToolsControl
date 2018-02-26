ab3e4a59489eb40deb82747d912373ef709190ad
伏维成 <fuweicheng@tomato.shop>
2018-02-26 14:00:22 +0800

    Initial Commit

---
+++ WorkToolsControl/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  WorkToolsControl
+//
+//  Created by fwc on 2018/2/26.
+//  Copyright © 2018年 fwc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
