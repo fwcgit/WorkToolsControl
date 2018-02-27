cabd5e0aed668d272b225b8b241349db1195f89d
伏维成 <fuweicheng@tomato.shop>
2018-02-27 10:25:58 +0800

    if

--- WorkToolsControl/main.c
+++ WorkToolsControl/main.c
@@ -8,20 +8,38 @@
 
 #include <stdio.h>
 #include "usart.h"
+#include <string.h>
 
 int main(int argc, const char * argv[]) {
   
     int fd = open_serial_port("/dev/ttyUSB0");
     init_seral_params(fd, 115200);
-    
     char data[3];
-    data[0] = 0x3b;
-    data[1] = 0x00;
-    data[2] = 0x0d;
+    char *input = (char*)malloc(sizeof(char)*10);
     
-    send_data(fd, data, 3);
-    
-    usart_close(fd);
+    while(1)
+    {
+        gets(input);
+        if(strstr(input, "off"))
+        {
+            data[0] = 0x3b;
+            data[1] = 0x00;
+            data[2] = 0x0d;
+            send_data(fd, data, 3);
+        }
+        else if(strstr(input, "on"))
+        {
+            data[0] = 0x3b;
+            data[1] = 0x01;
+            data[2] = 0x0d;
+            send_data(fd, data, 3);
+        }
+        else if(strstr(input, "exit"))
+        {
+            usart_close(fd);
+            break;
+        }
+    }
     
     printf("%d\r\n",fd);
     
